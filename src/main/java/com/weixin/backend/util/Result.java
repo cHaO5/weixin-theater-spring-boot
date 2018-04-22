45ad7887db5c709171a40f745844836430818d17
cHaO5 <quentin.hzk@gmail.com>
2018-04-22 23:49:45 +0800

    Add util result

---
+++ src/main/java/com/weixin/backend/util/Result.java
@@ -0,0 +1,52 @@
+package com.weixin.backend.util;
+
+public class Result {
+    private String code;
+    private String message;
+    private Object data;
+
+    public String getCode() {
+        return code;
+    }
+
+    private void setCode(ResultCode code) {
+        this.code = code.val();
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    private void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Result() {
+        this.setCode(ResultCode.SUCCESS);
+        this.setMessage("成功！");
+    }
+
+    public Result(ResultCode code) {
+        this.setCode(code);
+        this.setMessage(code.msg());
+    }
+
+    public Result(ResultCode code, String message) {
+        this.setCode(code);
+        this.setMessage(message);
+    }
+
+    public Result(ResultCode code, String message, Object data) {
+        this.setCode(code);
+        this.setMessage(message);
+        this.setData(data);
+    }
+}
